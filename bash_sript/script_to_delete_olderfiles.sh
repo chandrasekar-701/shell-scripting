9cb2168700e16d77532be669251088e4072c305c
chandrasekar k <mailto701mail@gmail.com>
2025-05-15 12:00:08 +0530

    Create script_to_delete_olderfiles.sh

---
+++ bash_sript/script_to_delete_olderfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "this is the script file to delete the files which are olderthan desired number of days"
+
+path=$1
+days=$2
+
+find $path -mtime +$days -delete
+
+echo "Files in path: $path older than $days days are deleted successfully"                                                                            
